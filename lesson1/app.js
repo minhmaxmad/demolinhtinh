89734da448f14d68f9a97f61a2e71fb8d0925009
minhmaxmad <minhntph43795@fpt.edu.vn>
2024-01-20 00:24:28 +0700

    day lan 1

---
+++ lesson1/app.js
@@ -0,0 +1,19 @@
+//variables
+// Có 3 cách khai báo var, let, const (2015-ecma6)
+// var
+// 1, re-declaration (tái khai báo)
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+//let
+
+//const
+
+// 2. Scope
+
+//
+let sv = "Nguyen Tuan Minh";
+let capitalized = sv.replace(sv.charAt(0), sv.charAt(0).toUpperCase());
+console.log(capitalized); // time kiem va thay the chu cai dau cua moi doan ki tu -> viet hoa
+
